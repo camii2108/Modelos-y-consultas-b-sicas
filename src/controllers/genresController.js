3959cbf0be69cf407f092c49803e41dfe3348067
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-03-30 12:57:42 -0300

    estaba pasando mal el objeto .then((data)) y  era .then((genres))

---
+++ src/controllers/genresController.js
@@ -0,0 +1,11 @@
+const db = require("../database/models");
+
+module.exports = {
+    list: (req, res) => {
+        /* Retorna la vista con todos los generos de la DB */
+        db.Genre.findAll()/* me devuelve un objeto promise */
+        .then((genres) => {
+            return res.render("genresList", {genres})
+        })
+    }
+}
--- src/database/models/Genre.js
+++ src/database/models/Genre.js
@@ -1,1 +1,33 @@
 /*Creando archivo Genre*/ 
+/* contiene una funcion que recibe dos parametros y puede ser anonima */
+module.exports = (sequelize, dataTypes) => {
+const alias = "Genre";
+const cols = {
+/*Van las constrains  o los nombres d elas porpiedades que pide sql INTEGER: entero*/
+    id: {
+        type: dataTypes.INTEGER(10).UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+       type: dataTypes.STRING(100) ,
+        allowNull: false,
+    },
+    ranking: {
+        type: dataTypes.INTEGER(10).UNSIGNED,
+        allowNull: false,
+    }, 
+    active: {
+        type: dataTypes.BOOLEAN,
+        allowNull: false,
+    },
+}
+const config = {
+    tableName: "genres",
+    timestamps: false,
+}
+
+const Genre = sequelize.define(alias, cols, config)
+return Genre;
+}
